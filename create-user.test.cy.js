e394d5ba2be31bb45072130316daaa0dabf70483
mbgshk <mbgshk@gmail.com>
2022-07-19 09:53:01 +0700

    HTTP Method Post

--- create-user.test.cy.js
+++ create-user.test.cy.js
@@ -1,2 +1,13 @@
 /// <reference types="cypress" />
 
+describe('Create New User', () => {
+    it('Add a new user', () => {
+        var user = {
+            "name": 'Muhammad Bagus Ishak',
+            "job": 'QA Engineer'
+        }
+        cy.request('POST', 'https:reqres.in/api/users', user).then((response) => {
+            expect(response.status).equal(201)
+        })
+    });
+});
