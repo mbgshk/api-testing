ce203dc9debf76eaa63cddebc45988aef2919708
mbgshk <mbgshk@gmail.com>
2022-07-19 10:32:12 +0700

    Submit tugas

--- create-user.test.cy.js
+++ create-user.test.cy.js
@@ -9,5 +9,11 @@ describe('Create New User', () => {
         cy.request('POST', 'https:reqres.in/api/users', user).then((response) => {
             expect(response.status).equal(201)
         })
+        cy.request('POST', 'https:reqres.in/api/users', user).then((respone) => {
+            expect(respone.body).to.have.property('name', 'Muhammad Bagus Ishak')
+        })
+        cy.request('POST', 'https:reqres.in/api/users', user).then((respone) => {
+            expect(respone.body).to.have.property('job', 'QA Engineer')
+        })
     });
 });
