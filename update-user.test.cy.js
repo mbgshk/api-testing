80349ce6e9a5397679c3753b11de3eb310eced5f
mbgshk <mbgshk@gmail.com>
2022-07-22 10:28:44 +0700

    Upload update-user.test.cy.js

---
+++ update-user.test.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Update User', () => {
+    it('Successfully update user', () => {
+        var user = {
+            "name": "Muhammad Bagus Ishak",
+            "job": "Test Engineer"
+        }
+        cy.request('PUT', 'https://reqres.in/api/users/2', user).then((response) => {
+            expect(response.status).equal(200)
+            expect(response.body.name).to.eq(user.name)
+        })
+    });
+});
